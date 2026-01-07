8f212507f34b3d306b1020dc1930c3144a3a0515
Dave Walker <david.walker5@icloud.com>
2026-01-07 14:25:39 +0000

    Added the Job Status entity, migration and management logic

---
+++ src/ShippingRecorder.Data/Migrations/20260107141718_JobStatus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShippingRecorder.Data.Migrations
+{
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    public partial class JobStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JOB_STATUS",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    parameters = table.Column<string>(type: "TEXT", nullable: true),
+                    start = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    end = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    error = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JOB_STATUS", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JOB_STATUS");
+        }
+    }
+}
