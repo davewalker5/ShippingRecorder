3bf6122e0b640cb4c9b061961697f3fa7462e22e
Dave Walker <david.walker5@icloud.com>
2026-01-13 14:38:15 +0000

    Improved validation on 'sighting search by vessel' page

--- src/ShippingRecorder.Mvc/Models/SightingSearchByVesselViewModel.cs
+++ src/ShippingRecorder.Mvc/Models/SightingSearchByVesselViewModel.cs
@@ -6,6 +6,8 @@ namespace ShippingRecorder.Mvc.Models
     public class SightingSearchByVesselViewModel : SightingSearchViewModelBase
     {
         [DisplayName("IMO")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "IMO must be 7 digits long")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "IMO must contain digits only")]
         [Required(ErrorMessage = "You must provide a vessel IMO")]
         public string IMO { get; set; }
     }
